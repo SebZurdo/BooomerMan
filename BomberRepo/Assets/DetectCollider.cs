197c71f6091da93b85742c4b95cc61793170c9f1
SebZurdo <seflorez2003@gmail.com>
2020-12-06 19:32:40 -0500

    2 Enemy Sprites added

---
+++ BomberRepo/Assets/DetectCollider.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DetectCollider : MonoBehaviour
+{
+    private void DetectColl(Collider2D col)
+    {
+        if (col.CompareTag("Player"))
+        {
+            Pickup();
+        }
+    }
+
+    void Pickup()
+    {
+        Debug.Log("Power up picked up");
+    }
+}
